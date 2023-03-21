d7ee236b4f7773245507aedc7f0609e1d5482a6d
Atul Y <99254634+ATUL9372@users.noreply.github.com>
2023-03-21 12:11:17 +0530

    Add sudo

--- docker-script.sh
+++ docker-script.sh
@@ -4,5 +4,5 @@
 
 apt update && upgrade -y
 
-apt install -y git iproute2 wget curl systemd nano net-tools htop tar gnupg gawk mawk grep sed make  openssh-server openssh-clien
+apt install -y git iproute2 wget curl systemd nano net-tools htop tar gnupg gawk mawk grep sed make  openssh-server openssh-client sudo
 
