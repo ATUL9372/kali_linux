5027068be34e5546135b3c318e95664103fbb0ce
Atul Y <99254634+ATUL9372@users.noreply.github.com>
2023-03-21 12:30:36 +0530

    Remove openssh-client

--- docker-script.sh
+++ docker-script.sh
@@ -4,5 +4,6 @@
 
 apt update && upgrade -y
 
-apt install -y git iproute2 wget curl systemd nano net-tools htop tar gnupg gawk mawk grep sed make  openssh-server openssh-client sudo systemctl systemd
+apt install -y git iproute2 wget curl systemd nano net-tools htop tar gnupg gawk mawk grep sed make  openssh-server  sudo systemctl systemd
 
+# openssh-client
