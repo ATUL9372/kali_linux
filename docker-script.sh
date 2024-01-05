b0cfc94b8953fbe85c6bf2417f4abf6d4d54b081
Atul Y <99254634+ATUL9372@users.noreply.github.com>
2024-01-05 14:01:34 +0530

    Update docker-script.sh

--- docker-script.sh
+++ docker-script.sh
@@ -9,4 +9,5 @@ apt install -y git iproute2 wget curl systemd nano net-tools htop tar gnupg gawk
 apt install system-config-printer system-config-printer-common system-config-printer-udev system-tools-backends systemd systemd-sysv systemd-timesyncd -y
 
 
+
 # openssh-client
